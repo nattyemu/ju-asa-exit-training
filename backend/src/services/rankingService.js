4fc2a14b004ea21b3d99587ba5980b38d42ed1ff
Natnael Bayu <140151286+nattyemu@users.noreply.github.com>
2026-09-25 21:22:44 +0300

    Update rankingService.js

--- backend/src/services/rankingService.js
+++ backend/src/services/rankingService.js
@@ -80,7 +80,7 @@ export const recalculateExamRankings = async (examId) => {
       message: `Recalculated rankings for ${allResults.length} results`,
     };
   } catch (error) {
-   console.log(error)
+   // console.log(error)
     throw new Error("Failed to recalculate rankings");
   }
 };
